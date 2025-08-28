4b24eef26108bc19d792a9563f450f4e0254cafc
aeonearth <alanlin_yu@hotmail.com>
2025-08-28 19:53:25 +0800

    Create 3269032f_script.sh

---
+++ 3269032f_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get update
+puppet resource package git ensure=present
+puppet resource package apache2 ensure=present
+puppet resource service apache2 ensure=running
+puppet resource file /operate/3269032f/ ensure=absent force=true
+puppet resource file /operate/3269032f/ ensure=directory
+cd /operate/3269032f/
+git clone https://github.com/aeonearth/3269032f-op-repo.git
+cp -p /operate/3269032f/3269032f-op-repo/index-op-3269032f.html /var/www/html/index.html
